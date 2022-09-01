4c494f384c26b1f12ed9fef10694dbf9b648e24a
jogi <jogi@datafinitygroup.com>
2022-08-31 17:56:22 -0700

    figured out the column not in source thing

--- colchk_required.py
+++ colchk_required.py
@@ -3,16 +3,28 @@ import pandas as pd
 df_states = pd.read_excel(r'C:\test\Lead Import Template Worldwide.xlsx', sheet_name="Territory State List")
 df = pd.read_excel(r"C:\test\testrawdata.xlsx")
 dfa = pd.read_excel(r'C:\test\Lead Import Template Worldwide.xlsx', sheet_name="Acceptable List of Values")
-def check_required_col(colchk,colres,datasrc,datatemplate,colchk1):          #this checks the required columns filtering out the blanks
+df_zip = pd.read_excel(r'C:\test\ZIP_Locale_Detail.xls')
+
+#zip code file creation
+
+df_zip.sheet_names
+
+
+
+def check_required_col(colchk,colres,datasrc,datatemplate,colchk1):
+    #this checks the required columns filtering out the blanks
 
     dftemp = datasrc[[colchk]].copy()
 
     dftemp1 = dftemp.copy()
+        #this is the source list no blanks
     dftemp1.dropna(inplace = True)
 
     dftempa = datatemplate[[colchk1]].copy()
+        #this is the library list no blanks
     dftempa.dropna(inplace = True)
     dftempa = dftempa.copy()
+    print(df_zip['ZIP CODE'])
 
     dftemp[colres] = dftemp1[colchk].isin(dftempa[colchk1])
     dftemp[colres].fillna("required", inplace=True)
@@ -24,4 +36,4 @@ def check_required_col(colchk,colres,datasrc,datatemplate,colchk1):          #th
 
 
 
-check_required_col('State','State Good',df,df_states,'Abbreviation')
+check_required_col('Zip','Zip Good',df,df_zip,'DELIVERY ZIPCODE')
