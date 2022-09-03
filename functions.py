dba24558775806d654d92e80531fcda65a6487a0
jogi <jogi@datafinitygroup.com>
2022-09-03 16:04:49 -0700

    zip code code

--- functions.py
+++ functions.py
@@ -77,12 +77,24 @@ def check_phone_col(colchk,colres,datasrc):
 #def zip_check(colchk,colres,datasrc):
 
 import pandas as pd
-df = pd.read_excel(r"C:\test\testrawdata.xlsx")
-df_zip = pd.read_excel(r'C:\test\ZIP_Locale_Detail.xls')
 
-df_zip_temp = pd.DataFrame()
-df_zip_temp['zip5'] = df_zip['PHYSICAL ZIP'].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5)
-df_zip_temp['zip10'] = df_zip['PHYSICAL ZIP'].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5) + '-' + (df_zip['PHYSICAL ZIP 4'].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5))
+#df = pd.read_excel(r"C:\test\testrawdata.xlsx")
+#df_zip = pd.read_excel(r'C:\test\ZIP_Locale_Detail.xls')
+
+#df_zip_temp = pd.DataFrame()
+#dfzt = pd.DataFrame()
+
+#df_zip = pd.read_excel(r'C:\test\ZIP_Locale_Detail.xls', sheet_name='ZIP_DETAIL')
+#df_zip_temp['zip5'] = df_zip.iloc[:,4].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5)
+#df_zip_temp['zip10'] = df_zip.iloc[:,9].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5) + '-' + (df_zip.iloc[:,10].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5))
+
+
+#df_zip = pd.read_excel(r'C:\test\ZIP_Locale_Detail.xls', sheet_name='Unique_ZIP_DETAIL')
+#dfzt['zip5'] = df_zip.iloc[:,4].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5)
+#print(dfzt['zip5'])
+
+#df_zip_temp.concat(df_zip['zip5'], dfzt['zip5'])
+#df_zip_temp['zip10'] = df_zip.concat([df_zip.iloc[:,9].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5) + '-' + (df_zip.iloc[:,10].astype(str).replace(r'\.0$', '', regex=True).str.zfill(5)), df_zip_temp['zip10']])
 
 #if not colchk in datasrc.columns:
 #    print(colchk + " column not in source")
@@ -97,8 +109,15 @@ df_zip_temp['zip10'] = df_zip['PHYSICAL ZIP'].astype(str).replace(r'\.0$', '', r
 #dftempa = datatemplate[[colchk1]].copy()
 #dftempa.dropna(inplace=True)
 #dftempa = dftempa.copy()
+#print(df['Zip'])
+#print(df_zip_temp)
 
-df['Zip Status'] = df['Zip'].isin(df_zip_temp())
+#df_zip_temp1 = (df_zip_temp.to_numpy().ravel().tolist())
+#print(df_zip_temp1)
+#df['Zip Status'] = df['Zip'].astype(str).isin(df_zip_temp['zip_mail'])
+#print(df['Zip'].astype(str).isin(df_zip_temp1))
+#print(df['Zip'].astype(str).isin(df_zip_temp).any().any())
+#print(df['Zip'].astype(str).isin(df_zip_temp.to_numpy().ravel().tolist()))
 #dftemp[colres].fillna("required", inplace=True)
 
-print(df['Zip Status'])
+#print(df['Zip Status'])
