ee77de2a146446bcf9d0f31b46e5fc8bdf9a4c67
jogi <jeff@binary-tech.com>
2023-01-10 21:58:49 -0800

    added zip lookup

--- List check/main.py
+++ List check/main.py
@@ -175,15 +175,16 @@ check_required_col('State','State Good',df,df_states,'Abbreviation')
 
 #check zip code length for 5 characters or 5+4 or Canadian zip
 engine = SearchEngine()
-df['Zip'] = df['Zip'].astype(str)
-#df['Zip'] = df['Zip'].replace(r".0", r"", regex=True)
-df['Zip'] = df['Zip'].apply(lambda x : str(x).zfill(5))
-df['Zip Status'] = df['Zip'].str.match("^[']?[0-9]{5}(?:-[0-9]{4})?$|([ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d)")
 zip = df['Zip']
 print(zip)
 zipcode = engine.by_zipcode(zip)
 print(zipcode.major_city)
 df['City Lookup'] = zipcode.major_city
+df['Zip'] = df['Zip'].astype(str)
+#df['Zip'] = df['Zip'].replace(r".0", r"", regex=True)
+df['Zip'] = df['Zip'].apply(lambda x : str(x).zfill(5))
+df['Zip Status'] = df['Zip'].str.match("^[']?[0-9]{5}(?:-[0-9]{4})?$|([ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d)")
+
 #df.loc[df['Zip'].str.match("^[0-9]{4}") == True, 'Zip'] = df.loc['Zip'].str.zfill(5)
 #[df['Zip'].str.match("^[0-9]{4}") == True, df['Zip']] = df['Zip'].str.zfill(5)
 
